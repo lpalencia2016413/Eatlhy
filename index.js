fb73f5492e9fd800c1734b7fe83e47502c4bd558
lpalencia2016413 <lpalencia-2016413@kinal.edu.gt>
2021-08-03 21:44:41 -0600

    Cambio heroku

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose =  require("mongoose")
+const app = require("./app")
+var usuarioControlador = require("./src/controladores/usuarios.controlador");
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb+srv://administrador:cbf7d8dddb@eathly.3ji8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log('Se encuentra conectado a la base de datos');
+    app.listen(process.env.PORT || 3000, function () {
+        console.log("Servidor corriendo en el puerto 3000");
+        usuarioControlador.admin();
+    })
+}).catch(err => console.log(err))
